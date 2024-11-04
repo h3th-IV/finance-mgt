880e6d6d8ae9725dcc4a43db216d1995b21faad0
Samuel Bonu <samuelbonux10@gmail.com>
2024-11-04 16:49:10 +0100

    directory

---
+++ utils/responses.js
@@ -0,0 +1,18 @@
+const errorResponse = (res, status_code, error, data = null) => {  
+    return res.status(status_code).json({
+      success: false,
+      error: error,
+      data: data || null,
+    });
+  };
+  
+  const successResponse = (res, status_code, message, data = null) => {
+    return res.status(status_code).json({
+      success: true,
+      message: message,
+      data: data || null,
+    });
+  };
+  
+  module.exports = { errorResponse, successResponse };
+  
