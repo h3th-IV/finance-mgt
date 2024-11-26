98082ad6e686ed89eb4ca81cb15f1b2e93c2d89a
Samuel Bonu <samuelbonux10@gmail.com>
2024-11-26 01:10:38 +0100

    chore

--- helpers/calcRepayment.helper.js
+++ helpers/calcRepayment.helper.js
@@ -1,20 +1,15 @@
 const calculateRepaymentPlan = (loanAmount, loanDuration, interestRate) => {
-    const monthlyInterestRate = interestRate / 100 / 12;
-    const totalMonths = loanDuration;
-    
-    //amortized monthly payment calc
-    const monthlyPayment = loanAmount * (monthlyInterestRate / (1 - Math.pow(1 + monthlyInterestRate, -totalMonths))); 
-    const totalPayment = monthlyPayment * totalMonths;
-
-    //calc total capital and total interest
+    const monthlyInterest = (interestRate / 100) * loanAmount;
+    const monthlyRepayment = loanAmount / loanDuration + monthlyInterest;
+    const totalPayment = monthlyRepayment * loanDuration;
     const totalCapital = loanAmount;
     const totalInterest = totalPayment - totalCapital;
 
     return {
-        monthlyPayment: monthlyPayment.toFixed(2),
+        monthlyPayment: monthlyRepayment.toFixed(2),
         totalPayment: totalPayment.toFixed(2),
-        totalCapital: totalCapital.toFixed(2), //total principal amount
-        totalInterest: totalInterest.toFixed(2), //total interest paid over the loan period
+        totalCapital: totalCapital.toFixed(2),
+        totalInterest: totalInterest.toFixed(2),
         duration: loanDuration,
     };
 };
